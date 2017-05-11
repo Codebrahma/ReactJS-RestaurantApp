6e8b64924e1006975e9d1c1b5b98b48c66212246
T Rahul Thampi <trahulthampi@gmail.com>
2017-05-11 16:06:48 +0530

    basic search component completed without styling

---
+++ app/modules/common/components/Search.js
@@ -0,0 +1,71 @@
+import React, { Component, PropTypes } from 'react';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedValue: props.defaultValue || '',
+    };
+  }
+
+  onSelect = (itemValue) => {
+    console.log(itemValue);
+  }
+
+  handleInputChange = (e) => {
+    const targetValue = e.target.value;
+
+    this.setState(prevProps => ({
+      selectedValue: targetValue !== prevProps.selectedValue ?
+        targetValue : prevProps.selectedValue,
+    }));
+  }
+
+  render = () => {
+    const { selectedValue } = this.state;
+    const { placeholder, options, rtl } = this.props;
+    const autoSuggestions = (
+      <ul className="search-suggestions">
+        {options.map((item) => (
+          <li
+            className="item"
+            key={item.value}
+            value={item.value}
+            onClick={e => this.onSelect(item.value, e)}
+          >
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className="search-wrapper">
+        <div className={`input-wrapper ${rtl ? 'inverted' : ''}`}>
+          <div className="search-icon">&#9906;</div>
+          <input
+            type="text"
+            value={selectedValue}
+            onChange={this.handleInputChange}
+            placeholder={placeholder}
+          />
+        </div>
+        {selectedValue && autoSuggestions}
+      </div >
+    );
+  }
+}
+
+Search.propTypes = {
+  placeholder: PropTypes.string,
+  defaultValue: PropTypes.string,
+  rtl: PropTypes.bool,
+  options: PropTypes.array,
+};
+
+Search.defaultProps = {
+  rtl: false,
+  placeholder: 'Search',
+};
+
+export default Search;
