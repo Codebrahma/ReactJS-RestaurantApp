d32930cec5e0cd89729705d5f795be9e4877bdf7
T Rahul Thampi <trahulthampi@gmail.com>
2017-05-12 14:16:06 +0530

    updated search basic component using react-autocomplete module

--- app/modules/common/components/Search.js
+++ app/modules/common/components/Search.js
@@ -1,6 +1,39 @@
 import React, { Component, PropTypes } from 'react';
+import Autocomplete from 'react-autocomplete';
 
 import '../styles/Search.scss';
+
+const styles = {
+  wrapperStyle: {
+    flex: '1',
+    height: '100%',
+  },
+  menuStyle: {
+    width: '100%',
+    maxHeight: '50%',
+    borderRadius: '3px',
+    boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+    background: 'rgba(255, 255, 255, 0.9)',
+    padding: '2px 0',
+    fontSize: '90%',
+    position: 'fixed',
+    overflow: 'auto',
+  },
+  item: {
+    color: '#777',
+    padding: '4px 15px',
+    cursor: 'pointer',
+  },
+  highlightedItem: {
+    color: '#555',
+    background: '#f9f9f9',
+    padding: '4px 15px',
+    cursor: 'pointer',
+  },
+  menu: {
+    border: 'solid 1px #ccc',
+  },
+};
 
 class Search extends Component {
   constructor(props) {
@@ -10,68 +43,61 @@ class Search extends Component {
     };
   }
 
-  onSelect = (itemValue) => {
-    console.log(itemValue);
+  /**
+   * Makes the search component searchable,
+   * returns the menu items whose string matches to the component input value
+   */
+  matchStateToTerm = (state, value) => (
+    state.label.toLowerCase().indexOf(value.toLowerCase()) !== -1 ||
+    state.value.toLowerCase().indexOf(value.toLowerCase()) !== -1
+  )
 
-    this.setState({
-      selectedValue: '',
-    });
-  }
-
-  handleInputChange = (e) => {
-    const targetValue = e.target.value;
-
-    this.setState(prevProps => ({
-      selectedValue: targetValue !== prevProps.selectedValue ?
-        targetValue : prevProps.selectedValue,
-    }));
-  }
-
-  render = () => {
+  render() {
+    const { dataSource, rtl, searchable, matchStateToTerm } = this.props;
     const { selectedValue } = this.state;
-    const { placeholder, options, rtl } = this.props;
-    const autoSuggestions = (
-      <ul className="search-suggestions">
-        {options.map((item) => (
-          <li
-            className="item"
-            key={item.value}
-            value={item.value}
-            onClick={e => this.onSelect(item.value, e)}
-          >
-            {item.label}
-          </li>
-        ))}
-      </ul>
-    );
+    const wrapperClassName = `search-wrapper ${rtl ? 'inverted' : ''}`;
 
     return (
-      <div className="search-wrapper">
-        <div className={`input-wrapper ${rtl ? 'inverted' : ''}`}>
-          <div className="search-icon">&#9906;</div>
-          <input
-            type="text"
-            value={selectedValue}
-            onChange={this.handleInputChange}
-            placeholder={placeholder}
-          />
+      <div className={wrapperClassName}>
+        <div className="search-icon">
+          <span>&#9906;</span>
         </div>
-        {selectedValue && autoSuggestions}
-      </div >
+        <Autocomplete
+          value={selectedValue}
+          items={dataSource}
+          getItemValue={(item) => item.label}
+          wrapperStyle={styles.wrapperStyle}
+          menuStyle={styles.menuStyle}
+          onSelect={value => this.setState({ selectedValue: value })}
+          onChange={(event, value) => this.setState({ selectedValue: value })}
+          {...searchable && {
+            shouldItemRender: matchStateToTerm || this.matchStateToTerm,
+          }}
+          renderItem={(item, isHighlighted) => (
+            <div
+              style={isHighlighted ? styles.highlightedItem : styles.item}
+              key={item.value}
+            >
+              {item.label}
+            </div>
+          )}
+        />
+      </div>
     );
   }
 }
 
 Search.propTypes = {
-  placeholder: PropTypes.string,
-  defaultValue: PropTypes.string,
-  rtl: PropTypes.bool,
-  options: PropTypes.array,
+  defaultValue: PropTypes.string, // Set defaulValue for search component.
+  dataSource: PropTypes.array,  // Format: [{ value: '', label: '' }]
+  rtl: PropTypes.bool,  // Inverts the placement of icon, if true.
+  searchable: PropTypes.bool, // Searches from the provided dataSource
+  matchStateToTerm: PropTypes.func, // Function for implementing searchable
 };
 
 Search.defaultProps = {
-  rtl: false,
-  placeholder: 'Search',
+  rtl: false, // Default placement of icon is left
+  searchable: true, // Default component is searchable
 };
 
 export default Search;
